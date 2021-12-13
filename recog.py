626f571473f49c87c63a872aa9e2152c6e51af6f
nspencerjackson <nspencerjackson@gmail.com>
2021-12-13 15:17:36 +0800

    updated code

--- recog.py
+++ recog.py
@@ -1,3 +1,10 @@
 import cv2
 import pytesseract
 
+pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread('Capture.jpg')
+size = img.shape
+
+width = int(size[1]) * 2
+height = int(size[0]) * 2
